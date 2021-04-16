2fad1617bbd026a0189fcd0f9a3278b9e26290bf
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2021-04-16 16:08:26 +0200

    if img has correct src and alt

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -12,4 +12,13 @@ describe('Component TripSummary', () => {
     expect(renderedLink).toEqual('/trip/' + expectedId);
     console.log(component.debug());
   });
+
+  it('if img has correct src and alt', () => {
+    const expectedId = 'abc';
+    const expectedImage = 'image.jpg';
+    const name = 'trip';
+    const component = shallow(<TripSummary id={expectedId} image={expectedImage} name={name} cost='123' days={3} tags={['']} />);
+    expect(component.find('img').prop('src')).toEqual(expectedImage);
+    expect(component.find('img').prop('alt')).toEqual(name);
+  });
 });

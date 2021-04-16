037d3a711f462fb277caf67b3fbaca34d717a0c4
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2021-04-16 15:36:42 +0200

    Add new file TripSummary.test.js to the project and run first test - rendering correct link

---
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import TripSummary from './TripSummary';
+
+describe('Component TripSummary', () => {
+
+  it('should render correct link', () => {
+    const expectedId = 'abc';
+    const component = shallow(<TripSummary id={expectedId} image='image.jpg' name='trip' cost='123' days={3} tags={['']} />);
+
+    const renderedLink = component.find('.link').prop('to');
+    expect(renderedLink).toEqual('/trip/' + expectedId);
+    console.log(component.debug());
+  });
+});

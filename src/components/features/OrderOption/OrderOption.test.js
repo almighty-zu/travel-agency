0aea5d34a871eb96b0b7fd7d985571656b0556c4
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2021-04-17 14:58:55 +0200

    Add new test to OrderOption.test - should return empty object if called without required props

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import OrderOption from 'OrderOption.js';
+import OrderOption from './OrderOption.js';
 
 describe('Component OrderOption', () => {
   it('Should render without crashing', () => {
@@ -8,4 +8,10 @@ describe('Component OrderOption', () => {
     expect(component).toBeTruthy();
     console.log(component.debug());
   });
+
+  it('should return empty object if called without required props', () => {
+    const component = shallow(<OrderOption />);
+    expect(component).toEqual({});
+  });
+
 });

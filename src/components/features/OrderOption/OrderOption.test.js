8da0a5bdf21d907466714eb8caa3c0070d89df38
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2021-04-17 16:30:09 +0200

    Add new test to OrderOption.test - component should render prop name in title

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -6,12 +6,20 @@ describe('Component OrderOption', () => {
   it('Should render without crashing', () => {
     const component = shallow(<OrderOption type='text' name='name' />);
     expect(component).toBeTruthy();
-    console.log(component.debug());
   });
 
   it('should return empty object if called without required props', () => {
     const component = shallow(<OrderOption />);
     expect(component).toEqual({});
   });
+
+  it('should render prop name in title', () => {
+    const name = 'options';
+    const title = 'title';
+    const type = 'number';
+    const component = shallow(<OrderOption type={type} name={name} title={title} />);
+    expect(component.find('h3').text()).toEqual(name); /*Consider why title selector didnt worked*/
+  });
+
 
 });

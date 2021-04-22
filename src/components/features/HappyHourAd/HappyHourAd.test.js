dad78e1a9446347a90fee2de174c520326f46c62
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2021-04-22 14:15:12 +0200

    Add new folder HappyHourAd

---
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import HappyHourAd from 'HappyHourAd.js';
+
+describe('component HappyHourAd', () => {
+  it('should render without crashing', () => {
+    const component = shallow(<HappyHourAd />);
+    expect(component).toBeTruthy();
+  });
+
+});
+
+
